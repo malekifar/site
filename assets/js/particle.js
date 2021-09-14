052ad544fcdb786db712e3b8f9e9a7024eeab3d2
Alireza Malekifar <86642515+malekifar@users.noreply.github.com>
2021-09-14 23:52:23 +0430

    Update particle.js

--- assets/js/particle.js
+++ assets/js/particle.js
@@ -13,7 +13,7 @@ particlesJS('particles-js', {
 			value: [ '#2EB67D', '#ECB22E', '#E01E5B', '#36C5F0' ]
 		},
 		shape: {
-			type: 'image',
+			type: ["circle", "triangle", "image"],
 			stroke: {
 				width: 0,
 				color: '#fff'
